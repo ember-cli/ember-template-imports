24c9040a5aca98d7d5da8fa95983c56724a39a1a
Chris Garrett <cgarrett@linkedin.com>
2021-02-23 20:56:14 -0800

    Initial Implementation
    Completes the initial implementation of this addon!

--- index.js
+++ index.js
@@ -1,5 +1,60 @@
 'use strict';
+let VersionChecker = require('ember-cli-version-checker');
 
 module.exports = {
   name: require('./package').name,
+
+  included() {
+    this._super.included.apply(this, arguments);
+
+    let emberChecker = new VersionChecker(this.project).for('ember-source');
+    let htmlbarsChecker = new VersionChecker(this.parent).for(
+      'ember-cli-htmlbars'
+    );
+
+    if (!emberChecker.gte('3.25.0')) {
+      throw new Error(
+        'ember-template-imports requires ember-source 3.25.0 or higher'
+      );
+    }
+
+    if (!htmlbarsChecker.gte('5.4.0')) {
+      throw new Error(
+        'ember-template-imports requires ember-cli-htmlbars 5.4.0 or higher as a peer dependency'
+      );
+    }
+
+    let addonOptions = this._getAddonOptions();
+
+    let emberCliHtmlbarsOptions = (addonOptions['ember-cli-htmlbars'] =
+      addonOptions['ember-cli-htmlbars'] || {});
+
+    emberCliHtmlbarsOptions._customInlineModules = {
+      'ember-template-imports': {
+        export: 'hbs',
+        useTemplateLiteralProposalSemantics: 1,
+      },
+
+      'TEMPLATE-TAG-MODULE': {
+        export: 'GLIMMER_TEMPLATE',
+        debugName: '<template>',
+        useTemplateTagProposalSemantics: 1,
+      },
+    };
+  },
+
+  _getAddonOptions() {
+    return (
+      (this.parent && this.parent.options) ||
+      (this.app && this.app.options) ||
+      {}
+    );
+  },
+
+  setupPreprocessorRegistry(type, registry) {
+    if (type === 'parent') {
+      let GjsCompiler = require('./lib/gjs-compiler');
+      registry.add('js', new GjsCompiler());
+    }
+  },
 };
---
+++ tests/integration/gjs-test.gjs
@@ -0,0 +1,52 @@
+import { module, test } from 'qunit';
+import { setupRenderingTest } from 'ember-qunit';
+import { render } from '@ember/test-helpers';
+import { precompileTemplate } from '@ember/template-compilation';
+import Component from '@glimmer/component';
+
+import GjsTest from 'dummy/components/gjs-test';
+
+module('tests/integration/components/gjs', function (hooks) {
+  setupRenderingTest(hooks);
+
+  test('it works', async function (assert) {
+    const Foo = <template>Hello, {{@name}}!</template>
+
+    await render(
+      precompileTemplate(`<Foo @name="world" />`, {
+        strictMode: true,
+        scope: { Foo },
+      })
+    );
+
+    assert.equal(this.element.textContent.trim(), 'Hello, world!');
+  });
+
+  test('it works with classes', async function (assert) {
+    class Foo extends Component {
+      greeting = 'Hello';
+
+      <template>{{this.greeting}}, {{@name}}!</template>
+    }
+
+    await render(
+      precompileTemplate(`<Foo @name="world" />`, {
+        strictMode: true,
+        scope: { Foo },
+      })
+    );
+
+    assert.equal(this.element.textContent.trim(), 'Hello, world!');
+  });
+
+  test('it works with a component that is a top-level default export', async function (assert) {
+    await render(
+      precompileTemplate(`<GjsTest />`, {
+        strictMode: true,
+        scope: { GjsTest },
+      })
+    );
+
+    assert.equal(this.element.textContent.trim(), 'Hello, world!');
+  });
+});
