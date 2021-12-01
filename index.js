116bc43df85acd9063dca6005148bb2fcd0876e6
Dan Freeman <dfreeman@salsify.com>
2021-12-01 15:07:36 +0100

    Update ember-cli-htmlbars and remove 5.x config logic

--- index.js
+++ index.js
@@ -9,9 +9,6 @@ module.exports = {
     this._super.included.apply(this, arguments);
 
     let emberChecker = new VersionChecker(this.project).for('ember-source');
-    let htmlbarsChecker = new VersionChecker(this.parent).for(
-      'ember-cli-htmlbars'
-    );
 
     if (!emberChecker.gte('3.25.0')) {
       throw new Error(
@@ -19,41 +16,9 @@ module.exports = {
       );
     }
 
-    if (!htmlbarsChecker.gte('5.4.0')) {
-      throw new Error(
-        'ember-template-imports requires ember-cli-htmlbars 5.4.0 or higher as a peer dependency'
-      );
-    }
-
-    let addonOptions = this._getAddonOptions();
-
-    let emberCliHtmlbarsOptions = (addonOptions['ember-cli-htmlbars'] =
-      addonOptions['ember-cli-htmlbars'] || {});
-
-    emberCliHtmlbarsOptions._customInlineModules = {
-      'ember-template-imports': {
-        export: 'hbs',
-        useTemplateLiteralProposalSemantics: 1,
-      },
-
-      'TEMPLATE-TAG-MODULE': {
-        export: 'GLIMMER_TEMPLATE',
-        debugName: '<template>',
-        useTemplateTagProposalSemantics: 1,
-      },
-    };
-
     this.templateCompilerPath = this.parent.addons
       .find((a) => a.name === 'ember-cli-htmlbars')
       .templateCompilerPath();
-  },
-
-  _getAddonOptions() {
-    return (
-      (this.parent && this.parent.options) ||
-      (this.app && this.app.options) ||
-      {}
-    );
   },
 
   setupPreprocessorRegistry(type, registry) {
