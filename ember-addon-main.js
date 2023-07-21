99ca6812ce4c07a992237236ac968a820e26df00
patrick <pa.trickjlp@gmail.com>
2023-07-21 09:56:13 +0200

    use content-tag

--- ember-addon-main.js
+++ ember-addon-main.js
@@ -1,12 +1,11 @@
 'use strict';
 require('validate-peer-dependencies')(__dirname);
 let VersionChecker = require('ember-cli-version-checker');
-let { addPlugin, hasPlugin } = require('ember-cli-babel-plugin-helpers');
 
 module.exports = {
   name: require('./package').name,
 
-  included(includer) {
+  included() {
     this._super.included.apply(this, arguments);
 
     let emberChecker = new VersionChecker(this.project).for('ember-source');
@@ -15,12 +14,6 @@ module.exports = {
       throw new Error(
         'ember-template-imports requires ember-source 3.27.0 or higher'
       );
-    }
-
-    let pluginPath = require.resolve('./src/babel-plugin');
-
-    if (!hasPlugin(includer, pluginPath)) {
-      addPlugin(includer, pluginPath);
     }
 
     // Used in ember-cli-htmlbars to get the location of templateCompiler without traversing this.addons (https://github.com/ember-cli/ember-cli-htmlbars/blob/6860beed9a357d5e948abd09754e8a978fed1320/lib/ember-addon-main.js#L264)
--- src/util.js
+++
@@ -1,6 +0,0 @@
-// ember-template-lint@4.14.0 landed a direct import of this file as `import * as util from 'ember-template-imports/src/util.js';`
-// in order to prevent users from getting errors when getting a floating version of `ember-template-imports` that includes this file
-// being rewritting in `.ts` we need to re-export from `lib/utils.js`.
-//
-// TODO: this should be removed in the next major version
-module.exports = require('../lib/util');
