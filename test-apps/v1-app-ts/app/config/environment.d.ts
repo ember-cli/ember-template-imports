1d83167d024edffc1b8413f0bfa1081e6a444089
NullVoxPopuli <199018+NullVoxPopuli@users.noreply.github.com>
2024-05-22 15:51:16 -0400

    Add test apps (addon, app) x (js, ts)

---
+++ test-apps/v1-app-ts/app/config/environment.d.ts
@@ -0,0 +1,14 @@
+/**
+ * Type declarations for
+ *    import config from 'v1-app-ts/config/environment'
+ */
+declare const config: {
+  environment: string;
+  modulePrefix: string;
+  podModulePrefix: string;
+  locationType: 'history' | 'hash' | 'none';
+  rootURL: string;
+  APP: Record<string, unknown>;
+};
+
+export default config;
